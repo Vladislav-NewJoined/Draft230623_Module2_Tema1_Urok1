3738ff31f62e17ceae94d44e3625bca6a1706a4c
Vladislav-NewJoined <sozin.vladislav@mail.ru>
2023-06-24 23:04:07 +0300

    Initial commit

---
+++ src/lessons/java/Draft0_Whole_Video_2_1_1.java
@@ -0,0 +1,33 @@
+package lessons.java;
+
+interface Animal {
+    void say();
+}
+public class Draft1_Task2_1_1 {
+    static int catSayCounter = 0;
+    public static void main(String[] args) {
+        Animal cat = new Animal() {
+            @Override
+            public void say () {
+                System.out.println("mew");
+                catSayCounter++;
+                System.out.println(catSayCounter);
+            }
+        };
+        Animal dog = new Animal() {
+            @Override
+            public void say () {
+                System.out.println("gav");
+            }
+        };
+        Animal duck = new Animal() {
+            @Override
+            public void say () {
+                System.out.println("krya");
+            }
+        };
+        cat.say();
+        dog.say();
+        duck.say();
+    }
+}
